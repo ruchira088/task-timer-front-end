5aa99124859b08f3572d16e5b477432a40da7b76
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-06-21 02:32:35 +1000

    Add Git hash display to page and update build script in package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Timer from "@/components/timer/Timer"
 import styles from "./page.module.scss"
+import classNames from "classnames"
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Timer/>
+      <div className={classNames(styles.hide, "git-hash")}>{process.env.NEXT_PUBLIC_GIT_HASH}</div>
     </div>
   )
 }
